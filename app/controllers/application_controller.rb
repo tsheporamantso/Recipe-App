5355d1709fcaa3ed40515e2ec84894e54cf2cd2c
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-05 15:08:46 +0200

    Authenticated user and add name file on sign_up registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
