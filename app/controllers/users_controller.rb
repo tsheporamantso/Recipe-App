6456e010068a5358d877ec8373fc8ef0936d934c
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-05 15:58:18 +0200

    Define show action for user controller that returns user with id params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find_by_id(params[:id])
+
+    return unless @user.nil?
+
+    redirect_to user_path
+  end
 end
