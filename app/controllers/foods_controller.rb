880452395afe07b6e88c84cb04055655d8b3c7b8
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-06 13:30:45 +0200

    Add new and create action on foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,4 +3,24 @@ class FoodsController < ApplicationController
     @user = User.find(params[:user_id])
     @foods = @user.foods 
   end
+
+  def new
+    @user = current_user
+    @food = @user.foods.build
+  end
+
+  def create
+    @food = current_user.foods.build(food_parama)
+    if @food.save
+      redirect_to user_foods_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
