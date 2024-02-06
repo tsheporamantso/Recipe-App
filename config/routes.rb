83e5afc5e1c3395df171b998f188e14a15d5b729
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-06 12:38:33 +0200

    Add foods resourceful route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :new, :create, :destroy]
     resources :recipes, only:[:index, :new, :show, :create, :destroy]
   end
 end
