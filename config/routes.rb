7da9826bc34f3b5dd612341cddcb995f2f87b624
Fombi Favour <fombifavour@gmail.com>
2024-02-06 14:24:15 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
   devise_for :users
   root to: "users#index"
 
   resources :users, only: [:index, :show] do
     resources :foods, only: [:index, :new, :create, :destroy]
     resources :recipes, only:[:index, :new, :show, :create, :destroy]
+    resources :public_recipes, only: [:index, :show]
   end
 end
