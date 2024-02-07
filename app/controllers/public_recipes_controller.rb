8e57e7c304285cad61598ded12ceeba8362ce324
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-07 14:24:00 +0200

    Resolve conflicts and accepted changes from collaborator

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @p_recipes = Recipe.where(public: true)
     @recipes = Recipe.where(public: true).order(updated_at: :desc)
   end
 end
