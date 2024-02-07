3a2ed7d8664a06c75c468b49162a014fa25e7fa0
Fombi Favour <fombifavour@gmail.com>
2024-02-07 22:09:27 +0100

    Update public_recipes_controller.rb

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,10 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.where(public: true).order(updated_at: :desc)
+    @foods = Food.joins(:recipe_foods).where(user_id: current_user.id)
+    @total = 0
+    @foods.each do |food|
+      @total += food.price
+    end
   end
 end
