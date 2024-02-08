eb20720522507a5ac65718c3e28b0dc71389f909
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-08 10:20:37 +0200

    Resolve n+1 query problem for public_recipes controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(public: true).order(updated_at: :desc)
+    @recipes = Recipe.includes([:user]).where(public: true).order(updated_at: :desc)
     @foods = Food.joins(:recipe_foods).where(user_id: current_user.id)
     @total = 0
     @foods.each do |food|
