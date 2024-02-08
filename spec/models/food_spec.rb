0e6cc5e1965e8dbbe2a440336f0ce2e3a5da51b9
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-08 11:13:44 +0200

    Add food validation test case

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with correct attributes' do
+      user = User.create(email: 'tgramantso@gmail.com', password: 'password123')
+      food = Food.new(name: 'CheeseCake', measurement_unit: 'kg', price: 5, quantity: 50, user:)
+      
+      expect(food).to be_valid
+    end
+  end
 end
