b0937d3761ec7df05d73185ca13ed1383f7bd311
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-08 15:35:53 +0200

    Add ActionView Helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+
+  config.include ActionView::Helpers::TextHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
