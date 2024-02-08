ddecf06094d284c2a409b163b97a99e30d794670
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-08 12:42:30 +0200

    Add user model validation spec test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(
+      name: 'Tshepo',
+      email: 'tgramantso@gmail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  end
+
+  it 'should be valid with valid attributes' do
+    expect(@user).to be_valid
+  end
+
+  it 'should not be valid without the name' do
+    @user.name = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'should not be valid without email' do
+    @user.email = nil
+    expect(@user).to_not be_valid
+  end
+
+  it 'should not be valid without the password' do
+    @user.password = nil
+    expect(@user).to_not be_valid
+  end
 end
