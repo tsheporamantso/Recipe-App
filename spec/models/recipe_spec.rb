f329a8c20e0ebde73e42d90e6a5834f73fc4fdea
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-08 12:17:21 +0200

    Add recipe model validation test cases

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(
+      email: 'tgramantso@gmail.com', 
+      password: 'password123')
+
+    @recipe = Recipe.new(
+      name: 'CheeseCake', 
+      preparation_time: 10, 
+      cooking_time: 5, 
+      description: 'Delicious with cream',
+      public: true, 
+      user: @user
+      )
+  end
+  describe 'validations' do
+    it 'should be valid with valid attributes' do
+      expect(@recipe).to be_valid
+    end
+
+    it 'should not be valid without the name' do
+      @recipe.name = nil
+      expect(@recipe).to_not be_valid
+    end
+
+    it 'should not be valid without description' do
+      @recipe.description = nil
+      expect(@recipe).to_not be_valid
+    end
+
+    it 'should not be valid without preparation time' do
+      @recipe.preparation_time = nil
+      expect(@recipe).to_not be_valid
+    end
+
+    it 'should not be valid with negative preparation time value' do
+      @recipe.preparation_time = -1
+      expect(@recipe).to_not be_valid
+    end
+
+    it 'should not be valid without cooking time' do
+      @recipe.cooking_time = nil
+      expect(@recipe).to_not be_valid
+    end
+
+    it 'should not be valid with negative cooking time value' do
+      @recipe.cooking_time = -1
+      expect(@recipe).to_not be_valid
+    end
+  end
 end
